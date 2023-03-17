d110c2b5a5b2ee6ee9e3f994c57b327c4458b9dd
rojail <128169362+rojail@users.noreply.github.com>
2023-03-17 20:42:52 +0300

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,8 @@
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+ 
+local Window = Library.CreateLib("Name", "RJTheme3")
+
+local Tab = Window:NewTab("Main")
+
+
